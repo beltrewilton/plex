d5d191ab8b8e95e78b6bc1aed26f0a0d267ea148
beltrewilton <beltre.wilton@gmail.com>
2024-11-13 15:32:55 -0400

    real bug is the long base64 images?

--- apps/plexcore/lib/data/repo.ex
+++ apps/plexcore/lib/data/repo.ex
@@ -2,8 +2,4 @@ defmodule Plex.Repo do
   use Ecto.Repo,
     otp_app: :plex,
     adapter: Ecto.Adapters.Postgres
-
-  def go(_) do
-    IO.puts("HE RRE HERE HERE ")
-  end
 end
--- config/runtime.exs
+++ config/runtime.exs
@@ -2,13 +2,14 @@ import Config
 
 IO.inspect(System.get_env("PLEX_CFG"), label: "PLEX_CFG")
 
+plex_cfg = System.get_env("PLEX_CFG")
+
 IO.inspect(File.cwd!, label: "current path")
 
 private_key_pem = File.read!("../../.certs/private_unencrypted.pem")
 Application.put_env(:plexgw, :private_key_pem, private_key_pem)
 
-### THE BUGGY ####
-# DotenvParser.load_file(plex_cfg)
+DotenvParser.load_file(plex_cfg)
 
 IO.inspect(System.get_env("USERNAME"), label: "USERNAME")
 IO.inspect(System.get_env("DBPASSWORD"), label: "DBPASSWORD")
@@ -20,12 +21,8 @@ config :plex, Plex.Repo,
   password: System.get_env("DBPASSWORD"),
   database: System.get_env("DATABASE"),
   hostname: System.get_env("HOSTNAME"),
-  pool_timeout: 50_000,
-  queue_target: 50_000,
-  queue_interval: 50_000,
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10,
-  after_connect: {Plex.Repo, :go, []}
+  pool_size: 10
 
 DotenvParser.load_file("../../config/plexgw.cfg")
 
@@ -39,8 +36,5 @@ config :plexgw, Plexgw.Repo,
   password: System.get_env("GW_DBPASSWORD"),
   database: System.get_env("GW_DATABASE"),
   hostname: System.get_env("GW_HOSTNAME"),
-  pool_timeout: 50_000,
-  queue_target: 50_000,
-  queue_interval: 50_000,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10

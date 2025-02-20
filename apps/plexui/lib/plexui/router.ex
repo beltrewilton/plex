5e2a91dde8a3358260995e9cd5ccc2ed647155dd
beltrewilton <beltre.wilton@gmail.com>
2025-02-20 17:57:28 -0400

    change

--- apps/plexui/lib/plexui/router.ex
+++ apps/plexui/lib/plexui/router.ex
@@ -4,11 +4,11 @@ defmodule Plexui.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
+    plug Corsica, origins: "*"
     plug :fetch_live_flash
     plug :put_root_layout, html: {Plexui.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug Corsica, origins: "*"
   end
 
   pipeline :api do
